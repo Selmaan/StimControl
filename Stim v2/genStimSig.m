41227f82eadc8d87ea7ae88a2abd1e7147dd4cb3
Selmaan <chettih@fas.harvard.edu>
2014-12-05 11:39:57 -0500

    bug fixing genStimSig
    Fixed various bugs, and modified so that AO sampling is a stimParam (but
    throws a warning if unspecified)

--- Stim v2/genStimSig.m
+++ Stim v2/genStimSig.m
@@ -1,17 +1,29 @@
-function [sigs, stimParams] = genStimSig(targ,sHz,stimParams)
+function [sigs, stimParams] = genStimSig(targ,stimParams)
 %
 % function to generate stimulation signal for a single target. Does not
 % handle prepositioning of mirrors or turning off pockels after stimulation
 %
-% genStimSig(targ,sHz,stimParams)
+% genStimSig(targ,stimParams)
+%
+% stimParams field should contain:
+% 'sHz' - AI sampling rate
+% 'dur' - Stimulation duration in seconds
+% 'rotSpeed' - rotational frequency of stimulation pattern
+% 'oscSpeed' - amplitude oscillation frequency of stim pattern
+% 'pockPow' - stimulation power, specified in raw voltage input to pockels
+% 'rateComp' - whether to use rate compensation of stimulation command signal
+% 'pockPulseFreq' - Frequency of pockels pulsing (0 for continuous stim)
+% 'pockPulseDuty' - Duty cycle of pockels square wave pulse, in percentage
 
 %% Error Checking and Input Handling
 if ~exist('stimParams','var')
     stimParams = struct;
 end
 
-if ~exist('sHz','var')
-    error('Analog Out Sampling Rate not specified'),
+if ~isfield(stimParams, 'sHz')
+    warning('Analog Out Sampling Rate not specified'),
+    fprintf('Using default AO sampling rate of 1e5 Hz\n'),
+    stimParams.sHz = 1e5;
 end
 
 if ~isfield(stimParams, 'dur')
@@ -46,24 +58,25 @@ end
 
 if ~isfield(stimParams, 'pockPulseDuty')
     if stimParams.pockPulseFreq > 0
-        stimParams.pockPulseDuty = 0.5;
-        fprintf('Using default Pockels Pulse Duty Cycle of 0.5\n'),
+        stimParams.pockPulseDuty = 50;
+        fprintf('Using default Pockels Pulse Duty Cycle Percentage of 50\n'),
     else
-        stimParams.pockPulseDuty = 1;
+        stimParams.pockPulseDuty = 100;
     end
 end
 
 %% Generate signals
 
 [sigs.xSig,sigs.ySig] = genSpiralSigs(targ.diameter, targ.offset, stimParams.dur,...
-    stimParams.rotSpeed, stimParams.oscSpeed, sHz, stimParams.rateComp)
-nSamples = length(xSig);
+    stimParams.rotSpeed, stimParams.oscSpeed, stimParams.sHz, stimParams.rateComp);
+nSamples = length(sigs.xSig);
 sigs.pockSig = stimParams.pockPow*ones(nSamples,1);
 
 % Create pulsed pockels signal if a positive frequency is specified
 if stimParams.pockPulseFreq > 0
-    t = 1/sHz:1/sHz:stimParams.dur;
+    t = reshape(1/stimParams.sHz:1/stimParams.sHz:stimParams.dur,[],1);
     pulseTimeBase = t .* (2*pi * stimParams.pockPulseFreq);
     pockPulse = square(pulseTimeBase,stimParams.pockPulseDuty);
     sigs.pockSig = sigs.pockSig .* pockPulse;
+    sigs.pockSig(sigs.pockSig<0) = 0;
 end
